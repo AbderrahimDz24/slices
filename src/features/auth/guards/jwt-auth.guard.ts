ef3bb79e8fb29a737d08c281136eb8d96cfd1c16
Abderrahim HELLACI <abderrahimdz24@gmail.com>
2026-06-01 08:51:25 +0100

    refactor(auth): update `IS_PUBLIC_KEY` import path for consistency

--- src/features/auth/guards/jwt-auth.guard.ts
+++ src/features/auth/guards/jwt-auth.guard.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Reflector } from '@nestjs/core';
-import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { IS_PUBLIC_KEY } from '@auth/decorators';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
